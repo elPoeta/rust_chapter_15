b705005ecd012d63d690556dc1c6bd533831d28e
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-30 10:24:27 -0300

    use Box smart pointer

--- smart_pointer_box/src/main.rs
+++ smart_pointer_box/src/main.rs
@@ -1,4 +1,17 @@
 #![allow(unused)]
 fn main() {
-    println!("Hello, world!");
+    {
+        let b = Box::new(5);
+        println!("b = {}", b);
+    }
+    {
+        use List::{Cons, Nil};
+        #[derive(Debug)]
+        enum List {
+            Cons(i32, Box<List>),
+            Nil,
+        }
+        let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+        dbg!(list);
+    }
 }
